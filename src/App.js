c89f28353e7eb92dde49829d6c6f784a4a173963
yonggwan <choiyk1225@gmail.com>
2020-07-26 18:27:50 +0900

    modify main text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          안녕하세요
+          나를 고치고, 푸시하면
+          git workflow 에 의해
+          <code>.github/workflows/cicd.yaml</code> 의 설정을 토대로
+          Job을 실행하여...
+          Github runner에의해 빌드 및 aws s3 버킷으로 빌드파일을 업로드합니다 짜잔.
         </p>
         <a
           className="App-link"
